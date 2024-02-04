ff33aaa8c83f738cfd2864c8cd65704ff13e9a7f
Jedrzej Golebiewski <jgolebiewski1@sheffield.ac.uk>
2024-02-04 05:54:45 +0000

    Add Sentry session replays to frontend

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-    config.dsn = 'https://efca601128c6e0a420b95caaceb523e3@o4506685834067968.ingest.sentry.io/4506685839704064' unless Rails.env.development? || Rails.env.test?
+    config.dsn = ENV["PUBLIC_SENTRY_DSN"] unless Rails.env.development? || Rails.env.test?
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   
     # Set traces_sample_rate to 1.0 to capture 100%
